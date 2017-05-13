2e7eca89a929af87b0de20cfa54a236e6fca8d71
David Whynot <davidmwhynot@gmail.com>
2017-05-13 18:54:41 -0400

    started server superglobal tutorial

---
+++ website2/server-info.php
@@ -0,0 +1,11 @@
+<?php
+  # $_SERVER superglobal
+  // Create Server Array
+  $server = [
+    'Host Server Name' => $_SERVER['SERVER_NAME']
+  ];
+  
+  echo $server['Host Server Name'];
+
+  // Create Client Array
+ ?>
