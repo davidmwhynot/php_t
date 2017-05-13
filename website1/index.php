2a490ee5aa888c1b995327903c3dd52f87af4f26
David Whynot <davidmwhynot@gmail.com>
2017-05-13 18:40:44 -0400

    started include tutorial

---
+++ website1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My Website1 | Home</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+    <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <footer>
+      <p>&copy; My Website1 2017</p>
+    </footer>
+  </body>
+</html>
