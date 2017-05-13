71f38c0264fc68d0dd21a275503f5704148c184a
David Whynot <davidmwhynot@gmail.com>
2017-05-13 14:29:29 -0400

    started arrays tutorial

---
+++ arrays.php
@@ -0,0 +1,12 @@
+<?php
+  #Array - variable that holds multiple values
+  /*
+    - Indexed
+    - Associative
+    - Multidimensional
+  */
+
+  #Indexed
+  $people = array('Kevin', 'Jeremy', 'Sara');
+  echo $people[1];
+ ?>
