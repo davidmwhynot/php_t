f0fcd10d368f6d69f8df56421b711a4e07bf20b2
David Whynot <davidmwhynot@gmail.com>
2017-05-13 19:11:37 -0400

    finished server superglobal tutorial

--- website2/server-info.php
+++ website2/server-info.php
@@ -2,10 +2,22 @@
   # $_SERVER superglobal
   // Create Server Array
   $server = [
-    'Host Server Name' => $_SERVER['SERVER_NAME']
+    'Host Server Name' => $_SERVER['SERVER_NAME'],
+    'Host Header' => $_SERVER['HTTP_HOST'],
+    'Server Software' => $_SERVER['SERVER_SOFTWARE'],
+    'Document Root' => $_SERVER['DOCUMENT_ROOT'],
+    'Current Page' => $_SERVER['PHP_SELF'],
+    'Script Name' => $_SERVER['SCRIPT_NAME'],
+    'Absolute Path' => $_SERVER['SCRIPT_FILENAME']
   ];
-  
-  echo $server['Host Server Name'];
 
+  print_r($server);
+  echo '<hr>';
   // Create Client Array
+  $client = [
+    'Client System Info' => $_SERVER['HTTP_USER_AGENT'],
+    'Client IP' => $_SERVER['REMOTE_ADDR'],
+    'Remote Port' => $_SERVER['REMOTE_PORT']
+  ];
+  print_r($client);
  ?>
