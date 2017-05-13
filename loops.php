4ad6a4c404c71b2ad5fb6b771b5b320317858b26
David Whynot <davidmwhynot@gmail.com>
2017-05-13 17:13:40 -0400

    started loops tutorial

---
+++ loops.php
@@ -0,0 +1,15 @@
+<?php
+  # LOOPS - Execute code set number of times
+  /*
+    for
+    while
+    do while
+    foreach
+  */
+  # for loop
+  # @params - init, condition, inc
+  for($i = 0; $i <= 10; ++$i) {
+    echo 'Number ' . $i;
+    echo '<br>';
+  }
+ ?>
