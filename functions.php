d39ded83897f30ba5e2bd4927f59c9e5d1d5dbdc
David Whynot <davidmwhynot@gmail.com>
2017-05-13 17:38:07 -0400

    finished function tutorial

--- functions.php
+++ functions.php
@@ -4,4 +4,30 @@
     echo 'Hello world';
   }
   simpleFunction();
+  echo '<br>';
+  function sayHello($name = 'World') {
+    echo "Hello $name.<br>";
+  }
+  sayHello('Joe');
+  sayHello('Bob');
+  sayHello();
+  function addNumbers($num1, $num2) {
+      return $num1 + $num2;
+  }
+  $sum = addNumbers(2,3);
+  echo $sum;
+  echo '<br>';
+  // passing arguments by reference
+  $myNum = 10;
+  function addFive($num) { // this wont actually change the value of myNum
+    $num += 5;
+  }
+  function addTen(&$num) {
+    $num += 10;
+  }
+  echo "Value: $myNum<br>";
+  addFive($myNum);
+  echo "Value: $myNum<br>";
+  addTen($myNum);
+  echo "Value: $myNum<br>";
  ?>
