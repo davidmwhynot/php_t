eea1545273a7b7a210d7b50b70485cc9a909597c
David Whynot <davidmwhynot@gmail.com>
2017-05-13 17:20:22 -0400

    finished loops tutorial

--- loops.php
+++ loops.php
@@ -12,4 +12,34 @@
     echo 'Number ' . $i;
     echo '<br>';
   }
+
+  #While loop
+  # @params - condition
+
+  $a=0;
+  while ($a <= 10) {
+    echo($a);
+    echo '<br>';
+    ++$a;
+  }
+
+  #do while
+  # @params - condition
+  $x = 0;
+  do {
+    echo $x;
+    echo '<br>';
+    ++$x;
+  } while ($x < 10);
+  #foreach
+  $people = ['Brad','Jose','William'];
+  foreach ($people as $person) {
+    echo $person;
+    echo '<br>';
+  }
+  $people = array('Brad' => 'brad@gmail.com','Jose' => 'jose@gmail.com','William' => 'william@gmail.com');
+  foreach ($people as $person => $email) {
+    echo $person . ': ' . $email;
+    echo '<br>';
+  }
  ?>
