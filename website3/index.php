6d49b980f839242c349035f2e592638b873c0407
David Whynot <davidmwhynot@gmail.com>
2017-05-13 21:40:42 -0400

    started contact form tutorial

---
+++ website3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="http://bootswatch.com/cosmo/bootstrap.min.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar=brand" href="index.php">My Website</a>
+        </div>
+      </div>
+    </nav>
+  </body>
+</html>
